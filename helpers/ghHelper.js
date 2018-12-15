1f7f3bb631575be6e358a2958e51cf7a6efc5dff
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-12-15 18:31:57 +0900

    test ghHelper.getPrs

---
+++ helpers/ghHelper.js
@@ -0,0 +1,10 @@
+var GitHub = require('github-api');
+// var github = new GitHub();
+// var repo = github.getRepo('denoland', 'deno');
+const getPrs = () => {
+  return [];
+}
+
+module.exports = {
+  getPrs: getPrs
+}
--- tests/ghHelper.js
+++ tests/ghHelper.js
@@ -2,9 +2,9 @@
 const ghHelper = require('../helpers/ghHelper');
 const assert = require('chai').assert;
 
-describe('test ghHelpers', function() {
-  context('ghHelper.getPrs', function() {
-    it('should return array', function() {
+describe('test ghHelpers', function () {
+  context('ghHelper.getPrs', function () {
+    it('should return array', function () {
       const prs = ghHelper.getPrs('denoland/deno');
       assert.isArray(prs);
     });
