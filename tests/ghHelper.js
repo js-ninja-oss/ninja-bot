2d4eb3cd049236787b4c317afd1160db09c60404
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-12-15 18:08:50 +0900

    add test getPrs

---
+++ tests/ghHelper.js
@@ -0,0 +1,12 @@
+// helper loads all scripts passed a directory
+const ghHelper = require('../helpers/ghHelper');
+const assert = require('chai').assert;
+
+describe('test ghHelpers', function() {
+  context('ghHelper.getPrs', function() {
+    it('should return array', function() {
+      const prs = ghHelper.getPrs('denoland/deno');
+      assert.isArray(prs);
+    });
+  });
+});
