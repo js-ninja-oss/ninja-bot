c1e22368669d0a97d64476e792e9f80012b48f02
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-12-25 18:16:18 +0900

    debug test

--- test/user.js
+++ test/user.js
@@ -20,23 +20,16 @@ describe('test user.js', function() {
     beforeEach(function() {
       return co(function*() {
         yield this.room.user.say('user1', `user github ${githubs[0]}`);
-        yield this.room.user.say('user1', 'user me');
         yield this.room.user.say('user2', `user github ${githubs[1]}`);
         yield this.room.user.say('user2', 'user all');
       }.bind(this));
     });
 
     it('should add github accounts', function() {
-      expect(this.room.messages).to.eql([
-        ['user1', `user github ${githubs[0]}`],
-        ['hubot', `I set your github account as @${githubs[0]}.`],
-        ['user1', 'user me'],
-        ['hubot', `\n      ID: user1\n      GitHub: @${githubs[0]}\n      PR(All Time): -1\n      PR Count(This Month): -1\n      PR(This Month):\n      \n    `],
-        ['user2', `user github ${githubs[1]}`],
-        ['hubot', `I set your github account as @${githubs[1]}.`],
-        ['user2', 'user all'],
-        ['hubot', `\n      ID: user1\n      GitHub: @${githubs[0]}\n      PR(All Time): -1\n      PR Count(This Month): -1\n      PR(This Month):\n      \n    \n--------\n\n      ID: user2\n      GitHub: @${githubs[1]}\n      PR(All Time): -1\n      PR Count(This Month): -1\n      PR(This Month):\n      \n    `],
-      ]);
+      const lastMessage = this.room.messages[this.room.messages.length - 1][1];
+      // NOTE: クロールに失敗して初期値のままだと"-1"を含んでいる
+      expect(lastMessage).to.include(githubs[0]);
+      expect(lastMessage).to.include(githubs[1]);
     });
   });
 });
