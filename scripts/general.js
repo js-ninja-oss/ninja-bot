eb99316411b3bbc09854174cd17f7815dda6ca7c
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-12-26 19:48:45 +0900

    add kick me?

--- scripts/general.js
+++ scripts/general.js
@@ -5,8 +5,18 @@ module.exports = robot => {
     return res.send('ninja!');
   });
 
-  robot.hear(/reset khatbehu/i, res => {
-    robot.brain.remove('users');
-    return res.send('reset!');
+  robot.hear(/kick me\?/i, res => {
+    const userId = res.message.user.id;
+    const user = User.find(robot.brain, userId);
+    let message;
+    const {name, prCountMonth} = user.github;
+    if (prCountMonth > 1) {
+      message = 'No! ';
+    } else {
+      message = 'Yes! ';
+    }
+    message += `${name}'s' PR count is ${prCountMonth} in this Month`;
+
+    return res.send(message);
   });
 };
