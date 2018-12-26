fd4af2c3964a58e851a887e6c5236c31765efb81
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-12-26 11:52:26 +0900

    update ask github

--- scripts/dm.js
+++ scripts/dm.js
@@ -2,9 +2,9 @@ const User = require('../models/user');
 
 module.exports = robot => {
   robot.hear(/dm ask github/i, res => {
-    User.allIds(robot.brain).forEach(slackId => {
-      if (slackId === 'UD08Z6LPP') { // TODO: remove condition
-        robot.messageRoom(slackId, 'Please tell me your GitHub acount by "user github AccountName"');
+    User.noGithub(robot.brain).forEach(user => {
+      if (user.id === 'UD08Z6LPP') { // TODO: remove condition
+        robot.messageRoom(user.id, 'Please tell me your GitHub acount by saying like "user github ggtmtmgg"');
       }
     });
   });
